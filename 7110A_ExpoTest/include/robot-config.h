0c05e1443874f32f425c0a9f6d1a9ce92f241ee5
tuwill-chs <100309163+tuwill-chs@users.noreply.github.com>
2023-01-28 17:04:12 -0600

    Expo program

---
+++ 7110A_ExpoTest/include/robot-config.h
@@ -0,0 +1,30 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller;
+extern motor BackLeft;
+extern motor BackRight;
+extern motor FrontLeft;
+extern motor FrontRight;
+extern motor MiddleLeft;
+extern motor MiddleRight;
+extern inertial Inertial;
+extern motor_group leftDrive;
+extern motor_group rightDrive;
+// extern encoder leftEncoder;
+// extern encoder rightEncoder;
+extern motor catapult;
+extern motor intake;
+extern digital_out expansion;
+extern digital_out cataBoost;
+extern rotation cataSense;
+extern digital_out blocker;
+extern 
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
